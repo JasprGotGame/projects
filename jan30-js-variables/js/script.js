6d2df2f56fd39a8df44975c0e8a0e39b87520887
David Lee <91358@stu.kingsburghigh.com>
2017-01-30 15:51:13 -0800

    javascript variables

---
+++ jan30-js-variables/js/script.js
@@ -0,0 +1,21 @@
+alert('this is an alert');
+
+
+var myNumberVar = 7;
+var myFloatVar = 5.2;
+var myStringVar = "David Lee";
+var myBooleanVar = true;
+var myArrayVar = ['one', 'stingray', 'banana', 8, true];
+var myObjectVar = {
+  make: 'Infinity',
+  model: 'G35',
+  color: 'white',
+  year: 2013
+};
+
+console.log(typeof myNumberVar);
+console.log(typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myFloatVar);
+console.log(myArrayVar);
+console.log(myObjectVar.model);
