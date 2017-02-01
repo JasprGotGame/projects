b3c896be691597a8b098f0eee09b43956eccf034
David Lee <91358@stu.kingsburghigh.com>
2017-02-01 15:51:22 -0800

    Updated portfolio + feb1 javascript math

---
+++ feb1-js-math/js/script.js
@@ -0,0 +1,35 @@
+var myObjectVar = {
+  brand: 'apple',
+  device: 'phone',
+  model: 'iPhone six',
+  color: 'space gray'
+};
+
+var myArrayVar = ['one', 'two', 'three', 4, false];
+
+var myStringVar = "This Is A String";
+
+var myNumberVar =18;
+
+var myFloatVar = 6.9;
+
+var myBooleanVar = false;
+
+console.log(myObjectVar);
+console.log(myArrayVar);
+console.log(myStringVar);
+console.log(myNumberVar);
+console.log(myFloatVar);
+console.log(myBooleanVar);
+
+
+var numOne = 1;
+var numTwo = 2;
+var equal = (numOne + numTwo);
+var subt = (numOne - numTwo);
+var mult = (numOne * numTwo);
+var divi = (numOne / numTwo);
+console.log(equal);
+console.log(subt);
+console.log(mult);
+console.log(divi);
