ba18b0178978af485844463afe9a707262ee30fa
David Lee <91358@stu.kingsburghigh.com>
2017-02-03 15:56:39 -0800

    friday feb 03

---
+++ feb02-js-variables/js/script.js
@@ -0,0 +1,43 @@
+// var externalJavascript = "wha da heck";
+// var camelCase;
+// var not_camel_case;
+// var $dollarCase;
+// var dollar$case;
+// var _underScore;
+//
+//
+// var firstVar;
+//
+// var variable var UppercaseCamel
+// var SUPERUPPERCASE;
+
+var myNumberObject = 3;
+
+var myNewNumberObject = myNumberObject.toString()
+
+// console.log(myNumberObject);
+// console.log(myNewNumberObject);
+
+var myStringObject = "This is a string object";
+var myOtherStringObject = 'THIS IS ALSO A STRING OBJECT';
+// var myNonWorkingStringObject = 'this wont work';
+var myPoorGrammer = 'david\'s basketball';
+
+console.log(myStringObject, myOtherStringObject);
+
+
+var myBooleanTrue = false;
+var myBooleanFalse = true;
+
+var a = 5;
+var b = 10;
+var c = a + b;
+var d = a / b;
+var e = a * b;
+var f = a - b;
+
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
---
+++ feb03-js-strings/js/script.js
@@ -0,0 +1,17 @@
+var firstString = 'a literal string';
+var secondString = new String("a string object");
+// var concatString = firstString.concat(secondString);
+var firstName = "David ";
+var lastName = "Lee ";
+var born = 1998;
+var year = 2020;
+var answer =  year - born;
+
+var fullname = firstName + " James " + lastName;
+
+console.log(fullname);
+
+var getIndex = firstString.indexOf('l', 8);
+
+console.log(getIndex);
+console.log(answer);
